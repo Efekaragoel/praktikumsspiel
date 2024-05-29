55854f483a613bf2ab7b18fc33d982336c49f909
Robin Kaiser <robin.n.kaiser@gmail.com>
2024-05-29 17:07:23 +0200

    wording geändert

--- server_script/start_server.bat
+++ server_script/start_server.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Abrufen der lokalen IP-Adresse
 for /f "tokens=13" %%i in ('ipconfig ^| findstr /r /c:"IPv4-Adresse" /c:"IPv4 Address"') do set "localIP=%%i"
-echo Lokale IP-Adresse: %localIP%
+echo IP-Adresse des Servers: %localIP%
 echo Schreibe dir jetzt die IP adresse ab!
 echo Drücke dann einen knopf, so dass der server starten kann!
 pause
