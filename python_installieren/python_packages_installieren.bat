f001a19831f07ea79a37fc6e19324a8e23ff75ab
Robin Kaiser <robin.n.kaiser@gmail.com>
2024-05-29 17:02:03 +0200

    python installer dazugelegt

---
+++ python_installieren/python_packages_installieren.bat
@@ -0,0 +1,6 @@
+@echo off
+echo alle benötigten packages werden jetzt installiert
+pip install pygame
+echo alle benötigten packages wurden installiert.
+echo du kannst jetzt das fenster schließen
+pause
